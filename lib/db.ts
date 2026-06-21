2e8355a83034c965c5c83cb844d9046614a6be52
Wonders243 <merveilleskatabisomwe@gmail.com>
2026-06-22 00:36:17 +0200

    feat: analytics event tracking

--- lib/db.ts
+++ lib/db.ts
@@ -3,5 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
