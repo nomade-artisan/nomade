c10437702dc0361971aeb33633ab9e128afdecd0
Wonders243 <merveilleskatabisomwe@gmail.com>
2026-06-22 11:31:45 +0200

    feat: analytics event tracking

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import MaintenanceBanner from "@/components/MaintenanceBanner";
 import { Analytics } from "@vercel/analytics/next";
 import Footer from "@/components/Footer";
+import CookieConsent from "@/components/CookieConsent";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.nomade-artisan.fr"),
@@ -70,6 +71,7 @@ export default function PublicLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Analytics />
+      <CookieConsent/>
       <Navbar />
       <MaintenanceBanner />
       <main className="flex-1">{children}</main>
